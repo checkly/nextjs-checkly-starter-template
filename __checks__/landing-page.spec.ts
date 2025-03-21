ad56b0cbae8d1b58d14c1251f29f25c39a383d59
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-03-21 00:56:19 -0700

    Move `test.use()` to `playwrightConfig`
    "Playwright Test did not expect test.use() to be called here." when it was in spec

--- __checks__/landing-page.spec.ts
+++ __checks__/landing-page.spec.ts
@@ -2,11 +2,6 @@ import { test, expect } from "@playwright/test"
 
 test("Landing page", async ({ page }) => {
   const baseURL = process.env.ENVIRONMENT_URL! || process.env.PRODUCTION_URL!
-  test.use({
-    extraHTTPHeaders: {
-      "x-vercel-protection-bypass": process.env.VERCEL_AUTOMATION_BYPASS_SECRET || ""
-    }
-  })
   const response = await page.goto(baseURL)
   expect(response?.status()).toBeLessThan(400)
   await page.screenshot({ path: "test-results/screenshot/landing-page.jpg" })
